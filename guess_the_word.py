37176a270859ded9bf90c2393c19f2562e05e160
AdamKuzara <ada2163629@maricopa.edu>
2024-11-13 16:09:09 -0700

    Update guess_the_word.py
    Finalized mostly everything. Comments explain everything more in-depth. Now it's time to test and make any polishes/fixups.

--- guess_the_word.py
+++ guess_the_word.py
@@ -9,11 +9,46 @@ def guessing_game_start():
     word = chosenWord()
     # 6 chances based on Hangman rules.
     chances = 6
-    # Tracks the guessed letters done by the user and will be used to prevent repeat letters.
     guessed_letters = set()
+    # The word is made into a list with each letter not guessed by the user correctly being shown as _.
     concealed_word = [letter if letter in guessed_letters else "_" for letter in word]
+    letter_count = len(set(word))
 
     print("Welcome to Hangman! You must guess a chosen random word one letter at a time! Good luck.")
-    print("Current word: ", " ".join(concealed_word))
+    print("Current word: ", " ".join(concealed_word)) # Joining will show the list of unknown letters for the user to play off.
+
+    while chances > 0:
+        # Lower() is used to avoid problems with case sensitivity.
+        user_guess = input("Enter a letter: ").lower()
+
+        if user_guess in guessed_letters:
+            print("You've already guessed that letter. Try again.")
+            # Continue is used to avoid making the user lose their turn/chance since it's a word they already guessed.
+            continue
+
+        guessed_letters.add(user_guess)
+        # This loop uses enumerate to go through each letter in the word list and gives both the index and letter. If the letter at i's position matches the user's guess then concealed_word is updated.
+        # Concealed word is updated to replace _ with the letter at the i position. This works even if two letterss are guessed and filled in at once, like p in Apple or l in dollar.
+        if user_guess in word:
+            for i, letter in enumerate(word):
+                if letter == user_guess:
+                    concealed_word[i] = user_guess
+            # word.count is used to return the number of times user_guess appears in the word by checking if it's greater than 0.
+            # letter_count is decremented when a unique letter is correctly guessed for the first time.
+            if word.count(user_guess) > 0:
+                letter_count -= 1
+            print(f"You're getting warmer. {letter_count} letters remaining.")
+            print("Current word: ", " ".join(concealed_word))
+        else:
+            chances -= 1
+            print(f"Incorrect guess. Chances remaining: {chances}")
+            print("Current word: ", " ".join(concealed_word))
+        # This is the win condition.
+        if "_" not in concealed_word:
+            print("Amazing job! You've guessed the word:", word)
+            break
+    # This is the lose condition.
+    if chances == 0:
+        print("Sorry, you've run out of chances. The word was:", word)
 
 guessing_game_start()
