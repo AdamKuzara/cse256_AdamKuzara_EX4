034d342f22cc36276d4b93fa012fee8ce42261f4
AdamKuzara <ada2163629@maricopa.edu>
2024-11-13 11:07:29 -0700

    Update guess_the_word.py
    Defined attributes and more specifically some of the rules and frameworks of the guessing game. Also gave an introduction along with feedback to the user of how many characters are in the word they're gonna guess.

--- guess_the_word.py
+++ guess_the_word.py
@@ -4,3 +4,16 @@ import random
 def chosenWord():
     words = ["apple", "rock", "boulder", "wealthy", "poverty", "education", "castle", "family", "rhinoceros", "whale", "dollar", "quartz"]
     return random.choice(words)
+
+def guessing_game_start():
+    word = chosenWord()
+    # 6 chances based on Hangman rules.
+    chances = 6
+    # Tracks the guessed letters done by the user and will be used to prevent repeat letters.
+    guessed_letters = set()
+    concealed_word = [letter if letter in guessed_letters else "_" for letter in word]
+
+    print("Welcome to Hangman! You must guess a chosen random word one letter at a time! Good luck.")
+    print("Current word: ", " ".join(concealed_word))
+
+guessing_game_start()
